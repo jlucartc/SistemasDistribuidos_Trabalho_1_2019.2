f2320375b658550086579079a4252f4f87965405
jlucartc <jlucartc@gmail.com>
2019-10-02 10:57:55 -0300

    Modificando ReceiverUDP para receber mensagens de todos os grupos e realizando testes para entender melhor o multicast. Anotações no slide.

--- src/Servidor/ReceiveUDP.py
+++ src/Servidor/ReceiveUDP.py
@@ -10,10 +10,11 @@ import sys
 
 class ReceiveUDP(Thread):
 
-    def __init__(self,host = "224.0.0.2",port = 9998):
+    def __init__(self,host = "224.0.0.2",port = 9998, listen_all = False):
         Thread.__init__(self)
         self.host = host
         self.port = port
+        self.listen_all = listen_all
         try:
             self.socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,socket.IPPROTO_UDP)
         except:
@@ -22,10 +23,13 @@ class ReceiveUDP(Thread):
 
     def receive(self):
         try:
-            group = socket.inet_aton(self.host)
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.socket.bind((self.host,self.port))
-            mreq = struct.pack('4sl', group,socket.INADDR_ANY)
+            if(self.listen_all == True):
+                self.socket.bind(('',self.port))
+            else:
+                self.socket.bind((self.host,self.port))
+            group = socket.inet_aton(self.host)
+            mreq = struct.pack("4sl",group,socket.INADDR_ANY)
             self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         except:
             print("Falha na criação do socket. Verifique se a porta "+str(self.port)+" já está sendo utilizada.")
--- src/Servidor/SendUDP.py
+++ src/Servidor/SendUDP.py
@@ -10,7 +10,7 @@ import sys
 
 class SendUDP:
 
-    def __init__(self,host = "224.0.0.1", port = 9990):
+    def __init__(self,host = "224.1.1.1", port = 9998):
         #super().__init__()
         self.host = host
         self.port = port
@@ -24,4 +24,4 @@ class SendUDP:
         while 1:
             msg = input("Digite a mensagem: ")
             self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-            self.socket.sendto(msg.encode(),("224.1.1.1",9998))
+            self.socket.sendto(msg.encode(),(self.host,self.port))
